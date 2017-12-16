a857214b49d2a4bc36a1304e9399c83bcb313e89
Christopher Lee <xchrislee@gmail.com>
2017-12-15 16:44:09 -0800

    Implement basic header layout

--- src/components/App/Header.js
+++ src/components/App/Header.js
@@ -3,16 +3,16 @@ import { Button, Menu } from 'semantic-ui-react';
 
 export default class Header extends Component {
   state = { activeItem: 'home' }
-  handleItemClick = (e, {mame}) => this.setState({ activeItem: name})
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
   render() {
     const { activeItem } = this.state;
     return (
       <div>
         <Menu pointing secondary>
-          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-          <Menu.Item name='my movies' active={activeItem === 'userMovies'} onClick={this.handleItemClick} />
-          <Menu.Item name='profile' active={activeItem === 'profile'} onClick={this.handleItemClick} />
-          <Menu.Menu position='right'>
+          <Menu.Item name="home" active={activeItem === 'home'} onClick={this.handleItemClick} />
+          <Menu.Item name="my movies" active={activeItem === 'userMovies'} onClick={this.handleItemClick} />
+          <Menu.Item name="profile" active={activeItem === 'profile'} onClick={this.handleItemClick} />
+          <Menu.Menu position="right">
             <Menu.Item>
               <Button >Logout</Button>
             </Menu.Item>
